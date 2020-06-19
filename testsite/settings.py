f26099ae930fc6a577c9c59b3900c94300aea7e8
zafaris <zafaris.dev@gmail.com>
2020-06-19 14:42:46 +0100

    Added static folder

--- testsite/settings.py
+++ testsite/settings.py
@@ -121,8 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATIC_ROOT = '/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
